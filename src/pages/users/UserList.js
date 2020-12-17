01f0dcd24acfc59cf06e2591fbca4ef4b04ec3ca
Dmytro Omelchuk <omeldm@gmail.com>
2020-12-17 22:08:49 +0200

    Fixed load more button with loader

--- src/pages/users/UserList.js
+++ src/pages/users/UserList.js
@@ -1,27 +1,16 @@
-import React, {useEffect} from 'react';
-import {useDispatch, useSelector} from "react-redux";
+import React from 'react';
+import {useSelector} from "react-redux";
 import {useHistory} from "react-router-dom";
-import {fetchUserList} from "../../store/actions";
 import {userListSelector} from "../../store/selectors";
 import {User} from "./User";
 import {Table} from "../../components/Table";
-import {Loader} from "../../components/Loader";
 
 export const UserList = () => {
-    const dispatch = useDispatch();
     const userList = useSelector(userListSelector);
     const history = useHistory();
 
-    useEffect(() => {
-        dispatch(fetchUserList());
-    }, [dispatch]);
-
     const handleSelectUser = user => {
         history.push(`/users/${user.id}`)
-    }
-
-    if (!userList || userList.length === 0) {
-        return <Loader/>
     }
 
     return (
--- src/pages/users/Users.js
+++ src/pages/users/Users.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {fetchUserList} from "../../store/actions";
+import {Loader} from "../../components/Loader";
+import {userListSelector} from "../../store/selectors";
 import {UserList} from "./UserList";
 import {LoadMore} from "./LoadMore";
 import './Users.css';
 
 export const Users = () => {
+    const dispatch = useDispatch();
+    const userList = useSelector(userListSelector);
+
+    useEffect(() => {
+        dispatch(fetchUserList());
+    }, [dispatch]);
+
+    if (!userList || userList.length === 0) {
+        return <Loader/>
+    }
+
     return (
         <div className='users'>
             <UserList/>
