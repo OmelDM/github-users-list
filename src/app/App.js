4ad1c1a07254da9539c50819cf6100f0b36b64ff
Dmytro Omelchuk <omeldm@gmail.com>
2020-12-17 16:49:50 +0200

    Added redux

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,27 @@
+import {applyMiddleware, compose, createStore} from "redux";
+import {Provider as ReduxProvider} from 'react-redux';
+import thunk from 'redux-thunk';
 import {BrowserRouter} from 'react-router-dom';
 import {Routes} from "./Routes";
+import {rootReducer} from "../store/rootReducer";
 
 import './App.css';
+
+const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+
+const enhancer = composeEnhancers(
+    applyMiddleware(thunk),
+);
+
+const store = createStore(rootReducer, enhancer)
 
 function App() {
     return (
         <BrowserRouter>
-            <Routes/>
+            <ReduxProvider store={store}>
+                <Routes/>
+            </ReduxProvider>
         </BrowserRouter>
     );
 }
